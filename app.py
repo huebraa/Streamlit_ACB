b635e924724f491a2f9d00fe832152508c2a1e82
huebraa <huebra.asier@gmail.com>
2024-11-18 19:32:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -227,7 +227,7 @@ def mostrar_jugadores_por_posicion(df_filtrado, equipo_seleccionado):
     """
 
     # Añadir texto a la parte superior de la imagen
-    ax.text(0.5, 1.05, comentario, ha="center", va="center", fontsize=14, color="black", weight='bold', wrap=True)
+    ax.text(0.5, 1.05, comentario, ha="center", va="center", fontsize=88, color="black", weight='bold', wrap=True)
     
     # Coordenadas ajustadas para cada posición en una lista simple
     posiciones = {
